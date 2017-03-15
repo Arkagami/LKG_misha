b5ed4bf4b5e2559dd84a1e86955f1dc5dc6cb1bb
Arkagami <mixno20@gmail.com>
2017-03-15 04:53:35 +0300

    Написал подсчет попаданий в определенные интервалы равной длины.

--- LKG_misha/LKG_misha.cpp
+++ LKG_misha/LKG_misha.cpp
@@ -4,7 +4,13 @@
 #include <stdlib.h>
 #include <conio.h>
 
-long long a = 4097, c = 421, m = 4294967296; //22695477
+
+long long a = 4097, c = 421, m = 4294967296, cv=32, n=m/cv, diapazon[32], buf=0; //22695477
+
+void launch() {
+	for (int yy = 0;yy < cv;yy++) diapazon[yy] = 0;
+}
+
 long long lkg(long long x) {
 	long long  f = (a*x + c) % m;
 	return f;
@@ -12,22 +18,27 @@ long long lkg(long long x) {
 
 
 int main() {
+	launch;
 	long long x, count, y;
-	printf("Start lkg.\n");
+	printf("Start lkg.\n...\n");
 		x = rand();
 		long long k = lkg(x);
 		count = 1;
 		y = -1;
 		x = k;
 		while (y != k) {
+			//if (count == 10000) break;
 			y = lkg(x);
+			buf = y/n;
+			diapazon[buf]++;
 			x = y;
 			count++;
-			if (count % 10000000 == 0) printf("%10lld\n", count);
+			//if (count % 1000000000 == 0) printf("%10lld\n", count);
 		}
-		count--;
+		//count--;
 		printf("End lkg.\n");
 		printf("Period - %lld\n", count);
+		for (int yy = 0;yy < cv;yy++) printf("%d - %10lld\n",yy,diapazon[yy]);
 	getchar();
 	return 0;
 }
