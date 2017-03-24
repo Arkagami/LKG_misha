7103143559687cf7ff8878a7ac9e90f4f9b1a393
Arkagami <mixno20@gmail.com>
2017-03-24 23:34:17 +0300

    Добавлена мощность, удалено лишнее.

--- LKG_misha/LKG_misha.cpp
+++ LKG_misha/LKG_misha.cpp
@@ -5,40 +5,56 @@
 #include <conio.h>
 
 
-long long a = 4097, c = 421, m = 4294967296, cv=32, n=m/cv, diapazon[32], buf=0; //22695477
+long long a = 79385237, //Множитель
+c = 421, //Сдвиг
+m = 4294967296, //Модуль
+cv=32, //Количество диапазонов
+n=m/cv, //Длина одного диапазона
+diapazon[32], buf=0, 
+kol = 100000; //Сколько чисел считать
 
+//Изначальное обнуление массива диапазонов
 void launch() {
 	for (int yy = 0;yy < cv;yy++) diapazon[yy] = 0;
 }
 
+//ЛКГ
 long long lkg(long long x) {
 	long long  f = (a*x + c) % m;
 	return f;
 }
 
+//Вычисление мощности
+long long power() {
+	long long b = 1, s=0;
+	while (b%m) {
+		b = b*(a - 1);
+		s++;
+	}
+	return s;
+}
+
 
 int main() {
 	launch;
-	long long x, count, y;
-	printf("Start lkg.\n...\n");
-		x = rand();
+	long long x = rand(), count, y;
+	printf("Power - %lld\n-------------------------------------------------\n", power());
 		long long k = lkg(x);
 		count = 1;
 		y = -1;
 		x = k;
-		while (y != k) {
-			//if (count == 10000) break;
-			y = lkg(x);
-			buf = y/n;
-			diapazon[buf]++;
-			x = y;
-			count++;
-			//if (count % 1000000000 == 0) printf("%10lld\n", count);
-		}
-		//count--;
-		printf("End lkg.\n");
-		printf("Period - %lld\n", count);
-		for (int yy = 0;yy < cv;yy++) printf("%d - %10lld\n",yy,diapazon[yy]);
-	getchar();
-	return 0;
+			for(long long q=0;q<kol;q++) {
+				y = lkg(x);
+				buf = y/n;
+				diapazon[buf]++;
+				x = y;
+				count++;
+			}
+			count--;
+			//printf("Period - %lld\n", count);
+			printf("Hi-square:\n");
+		for (int yy = 0;yy < cv;yy++) printf("%2d - %lld\n",yy+1,diapazon[yy]);
+		printf("-------------------------------------------------\n");
+		getchar();
+		return 0;
 }
