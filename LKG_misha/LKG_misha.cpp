4ccfdb0a29ab80e04d49a53c2b7a7f91e720d653
Arkagami <mixno20@gmail.com>
2017-03-30 15:06:28 +0300

    Написал критерий серий. Проходит все 5 тестов)

--- LKG_misha/LKG_misha.cpp
+++ LKG_misha/LKG_misha.cpp
@@ -12,23 +12,25 @@ cv = 31, //Количество диапазонов
 n = m / cv, //Длина одного диапазона
 diapazon[32], buf = 0,
 masOfX[6], //Массив начальных значений X
-kol = 100000, //Сколько чисел считать
+kol = 1000000, //Сколько чисел считать
 testKol = 5; //Количество тестов
 
-//Для Покер-Критерия:
-long long k = 5, //Количество последовательных чисел
-mas[6], //Массив с последовательными числами
-N = 100000, //Количество групп
-gr[6], //Массив количества групп с r различными числами
-r = 0, //Количество различных чисел
-v = 0, //Счетчик по массиву
-R = 0; //Счетчик по массиву вероятностей
 
-long double p[10]; //Массив вероятностей
+long long
+d = 7, //Наиболее подходящее число
+d_2[7][7], //Массив количества пар
+N = 100000; //Сколько чисел считать
+
+
 
 //Изначальное обнуление массива диапазонов
 void launch() {
 	for (int yy = 0;yy < cv;yy++) diapazon[yy] = 0;
+}
+
+//Изначальное обнуление массива пар
+void launches() {
+	for (int xy = 0;xy < d;xy++) for (int yy = 0;yy < d;yy++) d_2[xy][yy] = 0;
 }
 
 //ЛКГ
@@ -51,51 +53,30 @@ long long power() {
 //Хи-квадрат
 long double hi_square() {
 	long long hi = 0;
-	for (int d = 0;d < cv;d++) {
-		hi += (diapazon[d] * diapazon[d] * cv);
+	for (int dd = 0;dd < cv;dd++) {
+		hi += (diapazon[dd] * diapazon[dd] * cv);
 	}
 	return (double(hi) / double(kol) - double(kol));
 }
 
-//Хи-квадрат для Покер-Критерия
-long double hi_square_pocker() {
-	long double hi = 0;
-	for (int d = 0;d < k;d++) {
-		hi = hi + (powl(double(gr[d]), 2.0) / p[d]);
-	}
-	return ((hi / double(N*5)) - double(N*5)); 
-}
-
-
-//Подсчет различных чисел для Покер-Критерия
-long long skolko() {
-	long long rr = 0;
-	for (long long i = 0;i < k;i++) {
-		for (long long j = 0;j < i;j++) {
-			if ((__max(mas[i], mas[j])-__min(mas[i], mas[j])) < (m/k)) goto end;
+//Критерий серий
+long double kri_ser() {
+	long long hi = 0;
+	for (int dd = 0;dd < d;dd++) {
+		for (int dd1 = 0;dd1 < d;dd1++) {
+			hi += (d_2[dd][dd1] * d_2[dd][dd1] * d * d);
 		}
-		rr++;
-	end:;
 	}
-	return rr;
+	return (double(hi) / double(N) - double(N));
 }
-
-//Вычисления числа Стирлинга
-long long stirling(long long stir_k, long long stir_r) {
-	if ((stir_k == stir_r) && (stir_k >= 0)) return 1;
-	if ((stir_r == 0) && (stir_k > 0)) return 0;
-	return (stirling(stir_k - 1, stir_r - 1) + stir_r*stirling(stir_k - 1, stir_r));
-}
-
 
 int main() {
-	masOfX[0] = 745;
-	masOfX[1] = 2837377;
-	masOfX[2] = 37488842;
-	masOfX[3] = 419857238;
-	masOfX[4] = 2094658277;
+	masOfX[0] = 723;
+	masOfX[1] = 37482;
+	masOfX[2] = 2837375;
+	masOfX[3] = 419855238;
+	masOfX[4] = 20946582779;
 	long long x, count, y;
-
 
 	printf("Power - %lld\n-------------------------------------------------\n", power());
 
@@ -122,34 +103,18 @@ int main() {
 		printf("-------------------------------------------------\n");
 
 
-		
-		//Pocker-kriterii - dont working
-		for (long long q = 0;q < testKol;q++) {
-			k = 5;
-			x = masOfX[q];
-			for (long long i = 0;i < k;i++) gr[i] = 0;
-			for (long long group = 0;group < N;group++) {
-				for (long long i = 0;i < k;i++) { mas[i] = x; x = lkg(x); }
-				R=skolko();
-				gr[R-1]++;
+		printf("Kriteri' seri' must be between %.2llf and %.2llf\n", (d*d + sqrtl(2.0*powl(double(d),2.0))*(-1.64) + 2.0*powl(1.64,2.0)/3.0 -2.0/3.0), (d*d + sqrtl(2.0*powl(double(d), 2.0))*(1.64) + 2.0*powl(1.64, 2.0) / 3.0 - 2.0 / 3.0));
+		for (int g = 0;g < testKol;g++) {
+			launches();
+			x = masOfX[g];
+			long long y1 = 0, y2 = 0;
+			for (long long q = 0;q < N;q++) {
+				y1 = lkg(x);
+				y2 = lkg(y1);
+				d_2[y1 % d][y2 % d]++;
+				x = y2;
 			}
-
-			for (long long i = 0;i < k;i++) {
-				long long d = 1;
-				for (int j = 0;j <= i;j++) {
-					d *= (gr[i] - j);
-			}
-				p[i] = fabsl((double(d) / powl(double(gr[i]), double(k)))*double(stirling(k,i+1)));
-			}
-
-
-			int i = 0;
-			while (i < k - 1) {
-				if ((__max(p[i], p[i + 1]) - __min(p[i], p[i + 1])) < 0.001) { gr[i] = gr[i] + gr[i + 1]; p[i] = p[i] + p[i + 1]; for (int j = i + 2;j < k;j++) { gr[j - 1] = gr[j]; p[j - 1] = p[j]; } k--; } else i++;
-				
-			}
-
-			printf("Pocker = %.5llf\n", hi_square_pocker());
+			printf("Kriteri' seri' %d = %.2llf\n", g + 1, kri_ser());
 		}
 		printf("-------------------------------------------------\n");
 
